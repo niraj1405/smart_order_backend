26ba6bd7c3263d69e8c3467611b70d2bd518d8ff
Niraj Kumar <nirajkumarverma.78@gmail.com>
2026-03-02 05:46:30 +0000

    Added base package and main application class

---
+++ src/test/java/com/niraj/smartorder/backend/SmartOrderApplication.java
@@ -0,0 +1,12 @@
+package com.niraj.smartorder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartOrderApplication.class, args);
+    }
+}
